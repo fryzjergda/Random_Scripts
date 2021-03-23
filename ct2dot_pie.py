78bee0cca86e0b660c5989a6c19acc4dc2ec5bbf
twirecki <twirecki@genesilico.pl>
2021-03-23 13:08:18 +0100

    fake xml generator for rnaframework

--- ct2dot_pie.py
+++ ct2dot_pie.py
@@ -58,8 +58,8 @@ def bracket_to_pair(ss):
 
 def pair_to_bracket(pairs_list):
 
-#    db_list = [['(',')'],['[',']'],['<','>'],['{','}'],['A','a'],['B','b'],['C','c'],['D','d'],['E','e']]
-    db_list = [['(',')'],['[',']'], ['<','>'],['{','}']]
+    db_list = [['(',')'],['[',']'],['<','>'],['{','}'],['A','a'],['B','b'],['C','c'],['D','d'],['E','e']]
+#    db_list = [['(',')'],['[',']'], ['<','>'],['{','}']]
 #    db_list = [['(',')']]
     
     seq_len = len(ss_1) #len(sequence)    
@@ -68,116 +68,86 @@ def pair_to_bracket(pairs_list):
     for i in range(0,len(db_list)):
         array_list.append(np.zeros(shape=(len(pairs_list),seq_len)))
     
-    print(array_list)
 
     for i in range(0,len(pairs_list)):
         array_list[0][i, pairs_list[i][0]] = 1
         array_list[0][i, pairs_list[i][1]] = -1
 
-    print(array_list)
+#    print(array_list)
     
-    '''
-    for i in range(0,len(array_list)):
-        print("\n\nSTEP", i)
-        array = array_list[i]
-        print(array[2])
-        print(array[3])
-        sum_a = array[2]+array[3]
-        print(sum_a)
-        print(np.nonzero(array[2]))
-        pos1 = np.nonzero(array[2])[0][0]
-        pos2 = np.nonzero(array[2])[0][1]
-        print(pos1, pos2)
-        print(sum_a[pos1:pos2+1], "pos1")
-        print(sum(sum_a[pos1:pos2+1]))
-    '''
-
-
     
     for i in range(0,len(array_list)):
-        print("\n\nARRAY", i)
+#        print("\n\nARRAY", i)
         current_arr = array_list[i]
         c = 0
         while len(current_arr) >1 :
-            print("\nSTEP",c)
+#            print("\nSTEP",c)
             vec1 = current_arr[0]
-            print(vec1, "vec1")
+#            print(vec1, "vec1")
             
             vec2 =  current_arr[1]
-            print(vec2, "vec2")
+#            print(vec2, "vec2")
             sum_vec = vec1 + vec2
             try:
                 pos1 = np.nonzero(vec1)[0][0]
                 pos2 = np.nonzero(vec1)[0][-1]
-                print(np.nonzero(vec1))
-                print(pos1, pos2)
+#                print(np.nonzero(vec1))
+#                print(pos1, pos2)
                 sum_pos = sum(sum_vec[pos1:pos2+1])
                 if sum_pos == 0:
-                    print("merge")
+#                    print("merge")
                 
                     current_arr[1] = sum_vec
                     current_arr = np.delete(current_arr, (0), axis =0) 
-                    print(current_arr)
+#                    print(current_arr)
                 else:
                     print("go away")
                     array_list[i+1][c] = vec2
                     current_arr = np.delete(current_arr, (1), axis =0)
                     print(current_arr)
-                
+                    quit()	                
                 c +=1
-                print(array_list)
+#                print(array_list)
             except:
                 current_arr = np.delete(current_arr, (0), axis =0)
         array_list[i] = current_arr 
            
     print("\nFINAL")
     print(array_list)
+
+    list_brackets = []
     for i in range(0, len(array_list)):
         positions = np.where(array_list[i] == 1)[1]
         if len(positions) != 0:
-            print(positions)
+            #print(positions)
+            list_brackets.append(positions.tolist())
     
+    print(list_brackets)
+    new_ss = list("."*len(ss_1))
+    print(ss_1)
+    print(''.join(new_ss))    
+    for i in range(0, len(list_brackets)):
+        open = db_list[i][0]
+        close = db_list[i][1]
+        for k in range(0, len(pairs_list)):
+            for l in range(0, len(list_brackets[i])):
+                if pairs_list[k][0] == list_brackets[i][l]:
+                    new_ss[pairs_list[k][0]] = open
+                    new_ss[pairs_list[k][1]] = close
     
-    
-    
-    
+    print(''.join(new_ss))
         
     quit()
-    '''
-        for k in range(0,len(current_arr)-1):
-            print("\n\nSTEP",i,k)
-            vec1 = current_arr[k]
-            vec2 = current_arr[k+1]
-            sum_vec = vec1 + vec2
-            pos1 = np.nonzero(vec1)[0][0]
-            pos2 = np.nonzero(vec1)[0][1]
-            sum_pos = sum(sum_vec[pos1:pos2+1])
-            if sum_pos == 0:
-                print("merge")
-            else:
-                print("go away")
-        
-    '''
-        
-         
-#    print(np.any(array_list[0]))            
-#    array_list[1][2,11]= 9
     
-    '''
-    k = np.zeros(shape=(1, seq_len))
-    print(k)
-    k = np.append(k,[[0., 3., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]], axis = 0)
-    print(k)
-    k = np.append(k,[[0., 3., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 7.]], axis = 0)
-    print(k)
-    '''
     pass
+
 
 if __name__ == '__main__':
 
     #infile, ss_to_map_in = argument_parser()
     sequence = "xxxxxxxxxxxxxxxx"
-    ss_1 = "((([[.<<..)))]]...>>"
+    ss_1 = "(((<<.[[..)))>>...]]"
+    ss_1 = "......(..(.((((([...(((((.........................((((((.((................{................................)).))))))..((((((((....)))))))){{{...........)))))..{{{{{{{{<................))))).)[[)((...)).]]..((((]((...........}}}}.}}}}.....}}}....((((((.......))))))......[}..........((((.((..((...(((((.......)))))))..)).))))..........{...................A..........>)).))))..............(((((.............<<))))).............(...(.((.................]((([((...............BB.(((((((........C))))))).................)).)))..........}....{.....((((((((((.a...)))..)))))))..{{)){).....((((((........A>>..))))))...........<<)(..((((.....))))...D...........]((..........bb))...................[c..}..}}..............................((((}((.(((.((((....)))).....(((((((((........))))))))))))..)).)))).a.....).>>....(((((((((.(((.........((((((d..............))))))........))).)))))))))...(.{.]......................).............(((((((.(((...(((((....)))))...))).......))))))).....(([[.......))(...(......((((((.....))))))....(((((.(((.....))).)))))..................(((.(((((}(.......).))))).)))..............................(((((((.....)))))))........{{{{{....){{{)(((((....(]].((((((.((.......)).))))))...................)...)))).).}}}......}}}}}...."
     print(ss_1)
     print("\n\nBRACKET TO PAIR\n\n")
     p_list = bracket_to_pair(ss_1)
---
+++ fake_xml_generator.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import argparse
+from argparse import RawTextHelpFormatter
+import os  
+from textwrap import wrap
+
+
+def argument_parser():
+
+    parser = argparse.ArgumentParser(description=__doc__, prog='fake_xml_generator', formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-f", "--file", required=True, dest="file",
+                            help="Input file. One-line FASTA format.")
+
+    args = parser.parse_args() 
+
+    infile = args.file
+
+    return infile
+
+
+
+def read_fasta():
+    with open(infile, 'r') as f:  # read sequence from file
+
+        for line in f:
+          if line[0] !=">":
+            #print f.readline()
+            seq = line.strip().replace("U","T").replace("u","t").upper()
+
+    print(seq)
+    return seq
+    
+    
+
+def get_formatted_seq_react():
+    
+    seq_list = wrap(seq,60)
+
+    seq_f = ""
+    react_f = ""
+    for i in range(0, len(seq_list)):
+        seq_f += 24*" "+seq_list[i]+"\n"
+        react_f += 24*" "+"NaN,"*len(seq_list[i])+"\n"
+    
+    return seq_f, react_f.rstrip(",\n")
+    
+    
+    
+def generate_fake_xml_str():
+
+
+    fake_str = ""
+    
+    fake_str += '<?xml version="1.0" encoding="UTF-8"?>\n'
+    fake_str += '<data combined="FALSE" maxmutrate="1" maxumut="0.05" norm="Box-plot" offset="1000000000" reactive="ACGT" remap="0" scoring="Siegfried" tool="rf-norm" win="1000000000">\n'
+    fake_str += '        <transcript id="'+name+'" length="29903">\n'
+    fake_str += '                <sequence>\n'
+    fake_str += seq_formatted
+    fake_str += '                </sequence>\n'
+    fake_str += '                <reactivity>\n'
+    fake_str += react_formatted+"\n"
+    fake_str += '                </reactivity>\n'
+    fake_str += '        </transcript>\n'
+    fake_str += '</data>\n'
+
+
+    return fake_str    
+
+
+def write_output():
+
+    outfile = open(name+'.xml', "w")
+    outfile.write(result_str)
+    outfile.close
+
+
+
+if __name__ == '__main__':
+
+    infile = argument_parser()
+    
+    name = ''.join(infile.split(".")[:-1])
+    
+    print(name)
+    seq = read_fasta()
+    seq_formatted, react_formatted = get_formatted_seq_react()
+        
+    result_str = generate_fake_xml_str()
+    write_output()
+    
