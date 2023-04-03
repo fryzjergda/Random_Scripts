9add9063c955080ce517b7bc0f939838c57d3325
twirecki <twirecki@genesilico.pl>
2023-04-03 13:22:04 +0200

    added cutting to r2, fixed position of rsqure on graph

--- r2.py
+++ r2.py
@@ -9,6 +9,7 @@ from multiprocessing import cpu_count
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 
 def argument_parser():
@@ -39,20 +40,23 @@ def argument_parser():
 def calc_r2(y_test, y_predicted, outname):
 
     r2 =  r2_score(y_test, y_predicted)
-    print(r2)
+#    print(r2)
     
     
     
     fig, ax = plt.subplots()
     ax.scatter(y_test, y_predicted)
 #    ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-    ax.set_xlabel('Repetition 1')
-    ax.set_ylabel('Repetition 2')
+    ax.set_xlabel(r_file1)
+    ax.set_ylabel(r_file2)
     #regression line
 #    y_test, y_predicted = y_test.reshape(-1,1), y_predicted.reshape(-1,1)
-    ax.annotate("r-squared = {:.3f}".format(r2_score(y_test, y_predicted)), (0, 1))
+#    ax.annotate("r-squared = {:.3f}".format(r2_score(y_test, y_predicted)), (0, 1))
     ax.plot([y_test], LinearRegression().fit([y_test], [y_predicted]).predict([y_test]))
-
+    rsq = "r-squared = {:.3f}".format(r2_score(y_test, y_predicted))
+    print(rsq)
+    plt.title(rsq)
+    
     plt.savefig(outname+'_R2.png')
 
     
@@ -91,8 +95,13 @@ def read_reactivity(in_react):
     reactivities = reactivities.rstrip(";")
     reactivities = reactivities.split(";")
     reactivities = [float(i) for i in reactivities]
+    for i in range(len(reactivities)):
+        if reactivities[i] < 0 and reactivities[i] != -1:
+            reactivities[i] = 0.0
     #reactivities = [reactivities]
 
+#    reactivities = [ '%.3f' % elem for elem in reactivities ]
+    reactivities = list(np.around(np.array(reactivities),3))
 
 
     return reactivities
@@ -105,13 +114,36 @@ def cut_inputs(reactivities):
     cut_from = int(cut_range[0])
     cut_to = int(cut_range[1])
 
-    
+    new_reactivities = reactivities[cut_from-1:cut_to]
 #    react_list = reactivities.split(";")
-    new_reactivities = ";".join(reactivities[cut_from-1:cut_to])    
+#    new_reactivities = ";".join(reactivities[cut_from-1:cut_to])    
     
     return new_reactivities
 
 
+
+def remove_outliers(r1, r2):
+
+    
+    list_to_remove = []
+    
+    for i in range(len(r1)):
+        if r1[i] == -1.0:
+            list_to_remove.append(i)
+    
+    for i in range(len(r2)):
+        if r2[i] == -1.0:
+            list_to_remove.append(i)
+
+    indexes = list(set(list_to_remove))
+    
+    for index in sorted(indexes, reverse=True):
+        del r1[index]
+        del r2[index]
+    
+    
+    return r1, r2
+    
 
 if __name__ == '__main__':
 
@@ -127,8 +159,12 @@ if __name__ == '__main__':
         react_prof1 = cut_inputs(react_prof1)
         react_prof2 = cut_inputs(react_prof2)
     
-    print(react_prof1)
-    print(react_prof2)
+#    print(react_prof1)
+#    print(react_prof2)
+    
+    react_prof1, react_prof2 = remove_outliers(react_prof1, react_prof2)
+#    print(react_prof1)
+#    print(react_prof2)
     
     calc_r2(react_prof1,react_prof2, outname)
     
